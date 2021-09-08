79fd10285aad269779089e64e7c3f6f8c71f8810
vijay kumar <vijay113kumar@gmail.com>
2021-09-08 19:11:44 +0530

    User Registration , Login, Logout in Pyhton n Django

--- Django_Apps/Blogging/blogging_users/urls.py
+++ Django_Apps/Blogging/blogging_users/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns=[
     path("",views.index,name="index"),
-    path("register",views.register,name="register")
+    path("register",views.register,name="register"),
+    path("login",views.login,name="login"),
+    path("logout",views.logout,name="logout"),
+    path("post/<str:pk>",views.posts,name="post")
 ]
-
-
--- Django_Apps/Blogging/blogging_users/views.py
+++ Django_Apps/Blogging/blogging_users/views.py
@@ -1,12 +1,63 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 
 
 def register(request):
-    return render(request,"register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password == password2:
+            # check mail is exists or not?
+            if User.objects.filter(email=email).exists():
+                messages.info(request,"email already used.")
+                return redirect("register")
+            elif User.objects.filter(username = username).exists():
+                messages.info(request,"User name already used.")
+                return redirect("register")
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect("login")
+        else:
+            messages.info(request,"Confirm Password is not same.")
+    else:
+        return render(request,"register.html")
 
 def index(request):
     return render(request,"Index.html")
     #return HttpResponse(request,"<h1>Welcome Users</h1>")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username = username,password = password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Credientials Invalid.")
+            return redirect("login")
+
+    else:
+        return render(request,"login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
+
+
+def posts(request,pk):
+    return render(request,"post.html",{"pk":pk})
+
+
 
