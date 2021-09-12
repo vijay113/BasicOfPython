4f002d963ddcba9f2518b7e1cbc8f6787d3db971
vijay kumar <vijay113kumar@gmail.com>
2021-09-12 11:58:30 +0530

    Real time Chat App using Django and Python

---
+++ Django_Apps/My_Chat_Box/mychat/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Room)
+admin.site.register(models.Message)
+
+# Register your models here.
+
+
+
---
+++ Django_Apps/My_Chat_Box/mychat/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import datetime
+
+class Room(models.Model):
+    name = models.CharField(max_length=2000)
+
+
+
+class Message(models.Model):
+    value = models.CharField(max_length=100000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=100000)
+    room = models.CharField(max_length=100000)
+
+
+
