1e0bbd15039b67205f1c325993c73e413c257c19
vijay kumar <vijay113kumar@gmail.com>
2021-09-14 19:54:22 +0530

    Sales List View , Sale Detail page included in python

--- Django_Apps/Report_Management/reports/models.py
+++ Django_Apps/Report_Management/reports/models.py
@@ -1,3 +1,20 @@
+from django.db.models.fields import DateTimeField
+from profiles.models import Profile
 from django.db import models
+from django.utils import tree
+from profiles.models import Profile
 
-# Create your models here.
+class Report(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="reports",blank=True)
+    remarks = models.TextField()
+    author = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+
+
--- Django_Apps/Report_Management/sales/views.py
+++ Django_Apps/Report_Management/sales/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView
+
+from .models import Sale
+
 # Create your views here.
+
+def home_view(request):
+    return render(request,"sales/home.html",{})
+
+
+
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = "sales/main.html"
+    #context_object_name = "sales_list"
+
+
+class SaleDetailView(DetailView):
+    model = Sale
+    template_name = "sales/detail.html"
+
+
+
+
