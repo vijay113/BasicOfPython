adb5962b09cd64fff66ec39d81fc301db4406cb7
vijay kumar <vijay113kumar@gmail.com>
2021-10-29 19:13:12 +0530

    Initialized python Code of Resume app

--- BasicsOfPython/MyPractices/MyConceptsLearning.py
+++ BasicsOfPython/MyPractices/MyConceptsLearning.py
@@ -1,1 +1,25 @@
-print("Hello")
+class Base1():
+    def __init__(self):
+        self.str1 = "base1"
+        print("base1")
+
+        
+class Base2():
+    def __init__(self):
+        self.str2 = "base2"
+        print("base2")
+        
+class Derived(Base1,Base2):
+    def __init__(self):
+        Base1.__init__(self)
+        Base2.__init__(self)
+        print("Derived")
+        #super().__init__()
+        
+    def print_str(self):
+        print(self.str1,self.str2)
+        
+obj = Derived()
+obj.print_str()
+
+
